8fd0e5e5e307e1a7b35a63e6a78281f740b3a271
wawjr3d <wawjr3d@gmail.com>
2017-01-18 23:25:05 -0500

    copyright 2017

--- spec/client/client/ClientRequestSpec.js
+++ spec/client/client/ClientRequestSpec.js
@@ -276,7 +276,7 @@ describe("ClientRequest", function() {
 });
 
 // ----------------------------------------------------------------------------
-// Copyright (C) 2016 Bloomberg Finance L.P.
+// Copyright (C) 2017 Bloomberg Finance L.P.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
